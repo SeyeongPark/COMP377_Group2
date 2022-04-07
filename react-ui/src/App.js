e09a356372490b0bc8490212854880dea84f6e0a
etedali <etedali@gmail.com>
2022-04-07 16:31:31 -0400

    added the react ui

---
+++ react-ui/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+function App() {
+  const [data, setData] = useState([{}]);
+  useEffect(() => {
+    fetch("/members").then(
+      (res) => res.json()
+      ).then(
+        data => {
+        console.log(data);
+        setData(data);
+      })
+      .catch(error => {
+        alert(error)
+      });
+  }, []);
+  return (
+    <div>
+      {typeof data.members === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        data.members.map((member, i) => <p key={i}>{member}</p>)
+      )}
+    </div>
+  );
+}
+
+export default App;
